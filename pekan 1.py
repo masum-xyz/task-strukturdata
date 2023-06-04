8bc94a0aa0b4b25dfd30acfd7f789794477ab4f4
Moh Ma'sum Safrulihsan <79092153+Masum-XD@users.noreply.github.com>
2023-06-04 15:54:15 +0700

    Add files via upload

---
+++ pekan 1.py
@@ -0,0 +1,41 @@
+x = str ("Hello World")
+print (x)
+
+x = int (20)
+print (x)
+
+x = float (20.5)
+print (x)
+
+x = complex (1j)
+print (x)
+
+x = list (("apel", "pisang", "cherry"))
+print (x)
+
+x = tuple (("apel", "pisang", "cherry"))
+print (x)
+
+x = range(6)
+print (x)
+
+x = dict (nama = "John" , usia = 36)
+print (x)
+
+x = set (("apel", "pisang", "cherry"))
+print (x)
+
+x = frozenset (("apel", "pisang", "cherry"))
+print (x)
+
+x = bool (5)
+print (x)
+
+x = bytes (5)
+print(x)
+
+x = bytearray (5)
+print(x)
+
+x = memoryview(bytes(5))
+print(x)
